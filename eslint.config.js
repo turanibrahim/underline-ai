772f1423ad9f4754718afe3189a7958d82a8c331
Ibrahim Turan <hello@ibrahimturan.com>
2026-06-02 18:10:52 +0300

    feat: init project

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    vue: true,
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    pnpm: true,
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+    },
+  },
+  {
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+      'ts/no-explicit-any': 'error',
+    },
+  }
+)
